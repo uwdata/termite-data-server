5d3ca877fbf1bf58c3b9a438decb7b0368a80785
Jason Chuang <jcchuang@cs.stanford.edu>
2014-01-08 17:47:23 -0800

    Finished LDA API.

--- server_src/controllers/lda.py
+++ server_src/controllers/lda.py
@@ -5,54 +5,88 @@ import json
 
 def index():
 	identifier = {
-		'server_type' : 'lda',
-		'server_version' : '0.1',
-		'server_handlers' : [
-			'TermTopicMatrix',
+		'model_type' : 'lda',
+		'model_version' : '0.1',
+		'model_api' : [
+			'DocIndex',
 			'TermIndex',
-			'TopicIndex'
+			'TopicIndex',
+			'TermTopicMatrix',
+			'DocTopicMatrix'
 		]
 	}
 	return json.dumps( identifier, encoding = 'utf-8', indent = 2 )
 
+def GerDocIndex():
+	filename = os.path.join( request.folder, 'data/lda', 'doc-index.json' )
+	with open( filename ) as f:
+		content = json.load( f, encoding = 'utf-8' )
+	return content
+
+def GetTermIndex():
+	filename = os.path.join( request.folder, 'data/lda', 'term-index.json' )
+	with open( filename ) as f:
+		content = json.load( f, encoding = 'utf-8' )
+	return content
+
+def GetTopicIndex():
+	filename = os.path.join( request.folder, 'data/lda', 'topic-index.json' )
+	with open( filename ) as f:
+		content = json.load( f, encoding = 'utf-8' )
+	return content
+
 def GetTermTopicMatrix():
-	filename = os.path.join( request.folder, 'data', 'term-topic-matrix.txt' )
+	filename = os.path.join( request.folder, 'data/lda', 'term-topic-matrix.txt' )
 	with open( filename ) as f:
 		content = [ [ float(value) for value in line.split('\t') ] for line in f.read().decode( 'utf-8' ).splitlines() ]
 	return content
 
-def GerTermIndex():
-	filename = os.path.join( request.folder, 'data', 'term-index.txt' )
+def GetDocTopicMatrix():
+	filename = os.path.join( request.folder, 'data/lda', 'doc-topic-matrix.txt' )
 	with open( filename ) as f:
-		content = f.read().decode( 'utf-8' ).splitlines()
+		content = [ [ float(value) for value in line.split('\t') ] for line in f.read().decode( 'utf-8' ).splitlines() ]
 	return content
 
-def GetTopicIndex():
-	filename = os.path.join( request.folder, 'data', 'topic-index.txt' )
-	with open( filename ) as f:
-		content = f.read().decode( 'utf-8' ).splitlines()
-	return content
-
-def TermTopicMatrix():
+def DocIndex():
 	data = {
-		'content_type' : 'TermTopicMatrix',
-		'content_version' : '0.1',
-		'content' : GetTermTopicMatrix()
+		'api_type' : 'TermIndex',
+		'api_version' : '0.1',
+		'DocIndex' : GetDocIndex()
 	}
-	return json.dumps( data, encoding = 'utf-8' )
+	return json.dumps( data, encoding = 'utf-8', sort_keys = True )
 
 def TermIndex():
 	data = {
-		'content_type' : 'TermIndex',
-		'content_version' : '0.1',
-		'content' : GerTermIndex()
+		'api_type' : 'TermIndex',
+		'api_version' : '0.1',
+		'TermIndex' : GetTermIndex()
 	}
-	return json.dumps( data, encoding = 'utf-8' )
+	return json.dumps( data, encoding = 'utf-8', sort_keys = True )
 
 def TopicIndex():
 	data = {
-		'content_type' : 'TopicIndex',
-		'content_version' : '0.1',
-		'content' : GetTopicIndex()
+		'api_type' : 'TopicIndex',
+		'api_version' : '0.1',
+		'TopicIndex' : GetTopicIndex()
 	}
-	return json.dumps( data, encoding = 'utf-8' )
+	return json.dumps( data, encoding = 'utf-8', sort_keys = True )
+
+def TermTopicMatrix():
+	data = {
+		'api_type' : 'TermTopicMatrix',
+		'api_version' : '0.1',
+		'TermIndex' : GetTermIndex(),
+		'TopicIndex' : GetTopicIndex(),
+		'TermTopicMatrix' : GetTermTopicMatrix()
+	}
+	return json.dumps( data, encoding = 'utf-8', sort_keys = True )
+
+def DocTopicMatrix():
+	data = {
+		'api_type' : 'DocTopicMatrix',
+		'api_version' : '0.1',
+		'DocIndex' : GetDocIndex(),
+		'TopicIndex' : GetTopicIndex(),
+		'TermTopicMatrix' : GetDocTopicMatrix()
+	}
+	return json.dumps( data, encoding = 'utf-8', sort_keys = True )
