7f93172f15091a69ba6bafcbdaf184e25ec4c040
Jason Chuang <jcchuang@cs.stanford.edu>
2014-01-09 18:36:38 -0800

    Moved landing_src.

--- bin/setup.sh
+++ bin/setup.sh
@@ -78,8 +78,8 @@ function __setup_web2py__ {
 		
 		__create_folder__ $TOOLS_SUBPATH/applications/termite "    "
 		echo "    Setting up a default app..."
-		ln -s ../../../../server_src/termite/controllers $TOOLS_SUBPATH/applications/termite/controllers
-		ln -s ../../../../server_src/termite/views $TOOLS_SUBPATH/applications/termite/views
+		ln -s ../../../../landing_src/controllers $TOOLS_SUBPATH/applications/termite/controllers
+		ln -s ../../../../landing_src/views $TOOLS_SUBPATH/applications/termite/views
 		sed -i bkp "s/redirect(URL('welcome', 'default', 'index'))/redirect(URL('termite', 'default', 'index'))/g" $TOOLS_SUBPATH/gluon/main.py
 	else
 		echo "    Already available: $TOOLS_SUBPATH"
