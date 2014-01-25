1237e4e6194b139c05f5345fe997b7b97d5fa2dd
Jason Chuang <jcchuang@cs.stanford.edu>
2014-01-24 19:51:47 -0800

    Updated ImportCorpus script to generate a full web2py app.

--- bin/ImportCorpus.py
+++ bin/ImportCorpus.py
@@ -11,14 +11,17 @@ import json
 APPS_ROOT = 'apps'
 WEB2PY_ROOT = 'tools/web2py'
 
-class ImportMallet( object ):
+class ImportCorpus( object ):
 	
-	def __init__( self, apps_root, app_name, logging_level ):
+	def __init__( self, app_name, logging_level ):
 		self.app_name = app_name
-		self.app_path = '{}/{}'.format( apps_root, app_name )
-		self.app_data_corpus_path = '{}/{}/data/corpus'.format( apps_root, app_name )
+		self.app_path = '{}/{}'.format( APPS_ROOT, app_name )
+		self.app_data_corpus_path = '{}/{}/data/corpus'.format( APPS_ROOT, app_name )
+		self.app_controller_path = '{}/{}/controllers'.format( APPS_ROOT, app_name )
+		self.app_views_path = '{}/{}/views'.format( APPS_ROOT, app_name )
+		self.app_static_path = '{}/{}/static'.format( APPS_ROOT, app_name )
 		self.web2py_app_path = '{}/applications/{}'.format( WEB2PY_ROOT, app_name )
-		self.logger = logging.getLogger( 'ImportMallet' )
+		self.logger = logging.getLogger( 'ImportCorpus' )
 		self.logger.setLevel( logging_level )
 		handler = logging.StreamHandler( sys.stderr )
 		handler.setLevel( logging_level )
@@ -32,7 +35,7 @@ class ImportMallet( object ):
 		self.logger.info( '--------------------------------------------------------------------------------' )
 		
 		if not os.path.exists( self.app_path ):
-			self.logger.info( 'Creating output folder: %s', self.app_path )
+			self.logger.info( 'Creating app folder: %s', self.app_path )
 			os.makedirs( self.app_path )
 		if not os.path.exists( self.app_data_corpus_path ):
 			self.logger.info( 'Creating app data folder: %s', self.app_data_corpus_path )
@@ -43,6 +46,22 @@ class ImportMallet( object ):
 		
 		self.logger.info( 'Writing data to disk: %s', self.app_data_corpus_path )
 		self.SaveToDisk()
+
+		if not os.path.exists( self.app_controller_path ):
+			self.logger.info( 'Setting up app controllers: %s', self.app_controller_path )
+			os.system( 'ln -s ../../server_src/controllers {}'.format( self.app_controller_path ) )
+		
+		if not os.path.exists( self.app_views_path ):
+			self.logger.info( 'Setting up app views: %s', self.app_views_path )
+			os.system( 'ln -s ../../server_src/views {}'.format( self.app_views_path ) )
+		
+		if not os.path.exists( self.app_static_path ):
+			self.logger.info( 'Setting up app static folder: %s', self.app_static_path )
+			os.system( 'ln -s ../../server_src/static {}'.format( self.app_static_path ) )
+		
+		if not os.path.exists( self.web2py_app_path ):
+			self.logger.info( 'Adding app to web2py server: %s', self.web2py_app_path )
+			os.system( 'ln -s ../../../{} {}'.format( self.app_path, self.web2py_app_path ) )
 		
 		self.logger.info( '--------------------------------------------------------------------------------' )
 		
@@ -76,12 +95,10 @@ def main():
 	parser = argparse.ArgumentParser( description = 'Import a MALLET topic model as a web2py application.' )
 	parser.add_argument( 'app_name'     , type = str,                      help = 'Web2py application identifier'                   )
 	parser.add_argument( 'meta_file'    , type = str,                      help = 'Document metadata as tab-delimited or JSON file' )
-	parser.add_argument( '--apps_root'  , type = str, default = APPS_ROOT, help = 'Web2py application path.'                        )
 	parser.add_argument( '--logging'    , type = int, default = 20       , help = 'Override default logging level.'                 )
 	args = parser.parse_args()
 	
-	ImportMallet(
-		apps_root = args.apps_root,
+	ImportCorpus(
 		app_name = args.app_name,
 		logging_level = args.logging
 	).execute(
