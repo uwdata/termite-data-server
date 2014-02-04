64e3202efeab55f223d86600c00f813ae8a0d491
Jason Chuang <jcchuang@cs.stanford.edu>
2014-02-03 16:24:19 -0800

    More consistent script filenames. Added an import abstraction python class.

---
+++ bin/import_abstr.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+APPS_ROOT = 'apps'
+WEB2PY_ROOT = 'tools/web2py'
+SUBFOLDERS = [ 'models', 'views', 'controllers', 'static', 'modules' ]
+
+class ImportAbstraction( object ):
+
+	def __init__( self, app_name, app_model, app_desc ):
+		self.app_path = '{}/{}'.format( APPS_ROOT, app_name )
+		self.data_path = '{}/{}/data/{}'.format( APPS_ROOT, app_name, app_model )
+		self.web2py_path = '{}/applications/{}'.format( WEB2PY_ROOT, app_name )
+	
+		print '--------------------------------------------------------------------------------'
+		print 'Import "{}" as a web2py application...'.format( app_desc )
+		print '         app = {}'.format( app_name )
+		print '       model = {}'.format( app_model )
+		print '    app_path = {}'.format( self.app_path )
+		print '   data_path = {}'.format( self.data_path )
+		print ' web2py_path = {}'.format( self.web2py_path )
+		print '--------------------------------------------------------------------------------'
+		
+		if not os.path.exists( self.app_path ):
+			print 'Creating app folder: {}'.format( self.app_path )
+			os.makedirs( self.app_path )
+		
+		if not os.path.exists( self.data_path ):
+			print 'Creating data subfolder: {}'.format( self.data_path )
+			os.makedirs( self.data_path )
+		
+		for subfolder in SUBFOLDERS:
+			app_subpath = '{}/{}'.format( self.app_path, subfolder )
+			if not os.path.exists( app_subpath ):
+				print 'Linking subfolder: {}'.format( app_subpath )
+				os.system( 'ln -s ../../server_src/{} {}/{}'.format( subfolder, self.app_path, subfolder ) )
+		
+		filename = '{}/__init__.py'.format( self.app_path )
+		if not os.path.exists( filename ):
+			print 'Setting up __init__.py'
+			os.system( 'touch {}'.format( filename ) )
+
+	def AddToWeb2py( self ):
+		if not os.path.exists( self.web2py_path ):
+			print 'Adding app to web2py server: {}'.format( self.web2py_path )
+			os.system( 'ln -s ../../../{} {}'.format( self.app_path, self.web2py_path ) )
+		print '--------------------------------------------------------------------------------'
--- demo-20newsgroups.sh
+++ demo-20newsgroups.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 DEMO_PATH=demo-20newsgroups
-DOWNLOAD_PATH=$DEMO_PATH/download
 CORPUS_PATH=$DEMO_PATH/corpus
 MALLET_PATH=$DEMO_PATH/model-mallet
 MALLET_APP=20newsgroups_mallet
 TREETM_PATH=$DEMO_PATH/model-treetm
 TREETM_APP=20newsgroups_treetm
+TREETM_PATH=$DEMO_PATH/model-stmt
+TREETM_APP=20newsgroups_stmt
+GENSIM_PATH=$DEMO_PATH/model-gensim
+GENSIM_APP=20newsgroups_gensim
 
 function __create_folder__ {
 	FOLDER=$1
@@ -19,37 +22,7 @@ function __create_folder__ {
 }
 
 function __fetch_data__ {
-	echo "# Setting up the 20newsgroups dataset..."
-	__create_folder__ $DEMO_PATH "    "
-	
-	if [ ! -e "$DEMO_PATH/README" ]
-	then
-		echo "After a model is imported into a Termite server, you can technically delete all content in this folder without affecting the server. However you may wish to retain your model for other analysis purposes." > $DEMO_PATH/README
-	fi
-
-	if [ ! -d "$DOWNLOAD_PATH" ]
-	then
-		__create_folder__ $DOWNLOAD_PATH "    "
-		echo "    Downloading the 20newsgroups dataset..."
-		curl --insecure --location http://qwone.com/~jason/20Newsgroups/20news-18828.tar.gz > $DOWNLOAD_PATH/20news-18828.tar.gz
-		echo "    Setting up 20newsgroups information page..."
-		echo "<html><head><meta http-equiv='refresh' content='0;url=http://qwone.com/~jason/20Newsgroups/'></head></html>" > $DOWNLOAD_PATH/index.html
-	else
-		echo "    Already downloaded: $DOWNLOAD_PATH"
-	fi
-	
-	if [ ! -d "$CORPUS_PATH" ]
-	then
-		__create_folder__ $CORPUS_PATH "    "
-		echo "    Uncompressing the 20newsgroups dataset..."
-		tar -zxf $DOWNLOAD_PATH/20news-18828.tar.gz 20news-18828 &&\
-			mv 20news-18828/* $CORPUS_PATH &&\
-			rmdir 20news-18828
-	else
-		echo "    Already available: $CORPUS_PATH"
-	fi
-
-	echo
+	bin/fetch_20newsgroups.sh $DEMO_PATH
 }
 
 function __train_mallet__ {
@@ -96,6 +69,41 @@ function __import_treetm__ {
 	echo
 }
 
+function __train_stmt__ {
+	echo "# Training a STMT model..."
+	echo
+	echo "bin/train_stmt.py $CORPUS_PATH $STMT_PATH"
+	echo
+	bin/train_stmt.py $CORPUS_PATH $STMT_PATH
+	echo
+}
+
+function __import_stmt__ {
+	echo "# Importing a STMT model..."
+	echo
+	echo "bin/import_stmt.py $STMT_PATH $STMT_APP"
+	echo
+	bin/import_stmt.py $STMT_PATH $STMT_APP
+	echo
+}
+
+function __train_gensim__ {
+	echo "# Training a gensim LDA topic model..."
+	echo
+	echo "bin/train_gensim.py $CORPUS_PATH $GENSIM_PATH"
+	echo
+	bin/train_gensim.py $CORPUS_PATH $GENSIM_PATH
+	echo
+}
+function __import_gensim__ {
+	echo "# Importing a gensim LDA topic model..."
+	echo
+	echo "bin/import_gensim.py $GENSIM_APP $GENSIM_PATH/corpus.dict $GENSIM_PATH/output.model"
+	echo
+	bin/import_gensim.py $GENSIM_APP $GENSIM_PATH/corpus.dict $GENSIM_PATH/output.model
+	echo
+}
+
 if [ $# -gt 0 ]
 then
 	MODEL=$1
@@ -115,5 +123,19 @@ then
 	__fetch_data__
 	__train_treetm__
 	__import_treetm__
+elif [ "$MODEL" == "stmt" ] || [ "$MODEL" == "all" ]
+then
+	bin/setup_web2py.sh
+	bin/setup_stmt.sh
+	__fetch_data__
+	__train_stmt__
+	__import_stmt__
+elif [ "$MODEL" == "gensim" ] || [ "$MODEL" == "all" ]
+then
+	bin/setup_web2py.sh
+	bin/setup_gensim.sh
+	__fetch_data__
+	__train_gensim__
+	__import_gensim__
 fi
 bin/start_server.sh
