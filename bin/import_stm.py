90e918233660bbcce9e0e3bd25b15e76aba3342f
Jason Chuang <jcchuang@cs.stanford.edu>
2014-02-10 19:43:01 -0800

    Fixed bugs in import_stm. Working end-to-end on example data poliblogrun.RData.

--- bin/import_stm.py
+++ bin/import_stm.py
@@ -24,44 +24,56 @@ app.path.TopicIndex = "{DATA_PATH}/topic-index.json"
 load( file = "{MODEL_FILENAME}" )
 model = mod.out
 
-# Doc-Topic Matrix
-# Tab-seperated with no headers. Theta (D by K)
-data.DocTopicMatrix = model$theta
-write.table( data.DocTopicMatrix, file = app.path.DocTopicMatrix, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE )
-
-# Term-Topic Matrix
-# Tab-separated with no headers. Beta (V by K)
-data.TermTopicMatrix = exp( t( model$beta$logbeta[[1]] ) )
-write.table( data.TermTopicMatrix, file = app.path.TermTopicMatrix, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE )
-
 library( jsonlite )
 
+data.DocTopicMatrix = model$theta
+data.TermTopicMatrix = exp( t( model$beta$logbeta[[1]] ) )
+
 # Document Index
-temp.DocCount <- dim(model$beta$logbeta[[1]])[2]
+temp.DocCount <- nrow(model$theta)
 temp.DocIDs <- paste( "Document #", 1:temp.DocCount, sep = "" )
-temp.DocIndexValues <- cbind( temp.DocIDs )
-temp.DocIndexHeader <- c( "docID" )
+temp.DocIndex <- 1:temp.DocCount
+temp.DocIndexValues <- cbind( temp.DocIndex, temp.DocIDs )
+temp.DocIndexHeader <- c( "index", "docID" )
 colnames( temp.DocIndexValues ) <- temp.DocIndexHeader
 data.DocIndexJSON <- toJSON( as.data.frame( temp.DocIndexValues ), pretty = TRUE )
 write( data.DocIndexJSON, file = app.path.DocIndex )
 
 # Term Index
-temp.TermFreq <- apply( model$beta$logbeta[[1]], 2, sum )   # Cannot retrieve beta???
+temp.TermCount <- nrow( data.TermTopicMatrix )
+temp.TermFreq <- apply( data.TermTopicMatrix, 1, sum )
 temp.TermText <- model$vocab
-temp.TermIndexValues = cbind( temp.TermFreq, temp.TermText )
-temp.TermIndexHeader = c( "freq", "text" )
+temp.TermIndex <- 1:temp.TermCount
+temp.TermIndexValues = cbind( temp.TermIndex, temp.TermFreq, temp.TermText )
+temp.TermIndexHeader = c( "index", "freq", "text" )
 colnames( temp.TermIndexValues ) <- temp.TermIndexHeader
 data.TermIndexJSON <- toJSON( as.data.frame( temp.TermIndexValues ), pretty = TRUE )
 write( data.TermIndexJSON, file = app.path.TermIndex )
 
 # Topic Index
-temp.TopicFreq <- apply( model$beta$logbeta[[1]], 1, sum )   # Cannot retrieve beta???
-temp.TopicIndex <- 1:length(temp.TopicFreq)
-temp.TopicIndexValues = cbind( temp.TopicFreq, temp.TopicIndex )
-temp.TopicIndexHeader = c( "freq", "index" )
+temp.TopicCount <- ncol( data.TermTopicMatrix )
+temp.TopicFreq <- apply( data.TermTopicMatrix, 2, sum )
+temp.TopicIndex <- 1:temp.TopicCount
+temp.TopicIndexValues = cbind( temp.TopicIndex, temp.TopicFreq )
+temp.TopicIndexHeader = c( "index", "freq" )
 colnames( temp.TopicIndexValues ) <- temp.TopicIndexHeader
-data.TopicIndexJSON <- toJSON( as.data.frame( temp.TermIndexValues ), pretty = TRUE )
+data.TopicIndexJSON <- toJSON( as.data.frame( temp.TopicIndexValues ), pretty = TRUE )
 write( data.TopicIndexJSON, file = app.path.TopicIndex )
+
+# Doc-Topic Matrix
+# Tab-seperated with no headers. Theta (D by K)
+rownames( data.DocTopicMatrix ) <- temp.DocIDs
+colnames( data.DocTopicMatrix ) <- temp.TopicIndex
+data.DocTopicMatrixJSON <- toJSON( data.DocTopicMatrix, pretty = TRUE )
+write( data.DocTopicMatrixJSON, file = app.path.DocTopicMatrix )
+
+# Term-Topic Matrix
+# Tab-separated with no headers. Beta (V by K)
+rownames( data.TermTopicMatrix ) <- temp.TermText
+colnames( data.TermTopicMatrix ) <- temp.TopicIndex
+data.TermTopicMatrixJSON <- toJSON( data.TermTopicMatrix, pretty = TRUE )
+write( data.TermTopicMatrixJSON, file = app.path.TermTopicMatrix )
+
 """
 
 	def __init__( self, app_name, app_model = 'lda', app_desc = 'Structural Topic Model' ):
