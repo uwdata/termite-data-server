284b022c33955ac5f034fd8a604cd105570119f7
Radim Řehůřek <radimrehurek@seznam.cz>
2014-02-19 15:51:41 +0100

    don't load corpus into RAM with gensim

--- bin/train_gensim.py
+++ bin/train_gensim.py
@@ -5,7 +5,7 @@ import os
 import glob
 import argparse
 import re
-from gensim import corpora, models
+from gensim import corpora, models, utils
 
 DICTIONARY_FILENAME = 'corpus.dict'
 MODEL_FILENAME = 'output.model'
@@ -13,52 +13,60 @@ MODEL_FILENAME = 'output.model'
 WHITESPACES = re.compile( r'\W+' )
 LINEBREAKS = re.compile( r'[\t\n\x0B\f\r]+' )
 
-class TrainGensim( object ):
-	
-	def __init__( self ):
-		pass
-	
-	def Execute( self, corpus_path, model_path, num_topics ):
-		print '--------------------------------------------------------------------------------'
-		print 'Training a gensim topic model...'
-		print '      corpus = {}'.format( corpus_path )
-		print '       model = {}'.format( model_path )
-		print '  num_topics = {}'.format( num_topics )
-		print '--------------------------------------------------------------------------------'
-		
-		if not os.path.exists( model_path ):
-			print 'Creating model folder: {}'.format( model_path )
-			os.makedirs( model_path )
-		
-		# Based on example code provided by Samuel Rönnqvist
-		documents = []
-		if os.path.isdir( corpus_path ):
+
+class TermiteCorpus(corpora.TextCorpus):
+	def get_texts(self):
+		total_docs = 0
+		if os.path.isdir( self.input ):
 			# Read two levels of files
 			filenames = []
-			filenames += glob.glob( '{}/*'.format( corpus_path ) )
+			filenames += glob.glob( '{}/*'.format( self.input ) )
 			for filename in filenames:
 				if os.path.isdir( filename ):
 					filenames += glob.glob( '{}/*'.format( filename ) )
 			for filename in filenames:
 				if not os.path.isdir( filename ):
-					with open( filename ) as f:
+					with utils.smart_open( filename ) as f:
 						tokens = WHITESPACES.split( LINEBREAKS.sub( ' ', f.read().decode( 'utf-8', 'ignore' ) ).strip() )
-						documents.append( tokens )
+						yield tokens
+						total_docs += 1
 		else:
-			with open( corpus_path ) as f:
+			with utils.smart_open( self.input ) as f:
 				for line in f:
 					tokens = WHITESPACES.split( line[:-1].decode( 'utf-8', 'ignore' ) )
-					documents.append( tokens )
+					yield tokens
+					total_docs += 1
 
-		# Generate gensim dictionary
-		dictionary = corpora.Dictionary( documents )
-		corpus = [ dictionary.doc2bow( tokens ) for tokens in documents ]
-		model = models.LdaModel( corpus, id2word = dictionary, num_topics = num_topics )
-		
+		self.length = total_docs
+
+
+class TrainGensim( object ):
+
+	def __init__( self ):
+		pass
+
+	def Execute( self, corpus_path, model_path, num_topics, num_passes ):
+		print '--------------------------------------------------------------------------------'
+		print 'Training a gensim topic model...'
+		print '      corpus = {}'.format( corpus_path )
+		print '       model = {}'.format( model_path )
+		print '  num_topics = {}'.format( num_topics )
+		print '  num_passes = {}'.format( num_passes )
+		print '--------------------------------------------------------------------------------'
+
+		if not os.path.exists( model_path ):
+			print 'Creating model folder: {}'.format( model_path )
+			os.makedirs( model_path )
+
+		# Generate gensim objects
+		corpus = TermiteCorpus( corpus_path )
+		corpus.dictionary.filter_extremes()  # remove words that are too frequent/too infrequent, stopwords
+		model = models.LdaModel( corpus, id2word = corpus.dictionary, num_topics = num_topics, passes = num_passes )
+
 		filename = '{}/{}'.format( model_path, DICTIONARY_FILENAME )
 		print 'Saving dictionary to disk: {}'.format( filename )
-		dictionary.save( filename )
-		
+		corpus.dictionary.save( filename )
+
 		filename = '{}/{}'.format( model_path, MODEL_FILENAME )
 		print 'Saving model to disk: {}'.format( filename )
 		model.save( filename )
@@ -68,10 +76,11 @@ def main():
 	parser.add_argument( 'corpus_path'  , type = str , help = 'Input text corpus'              )
 	parser.add_argument( 'model_path'   , type = str , help = 'Output model'                   )
 	parser.add_argument( '--topics'     , type = int , help = 'Number of topics', default = 20 )
+	parser.add_argument( '--passes'     , type = int , help = 'Training passes', default = 1 )
 	args = parser.parse_args()
-	
+
 	train = TrainGensim()
-	train.Execute( args.corpus_path, args.model_path, args.topics )
+	train.Execute( args.corpus_path, args.model_path, args.topics, args.passes )
 
 if __name__ == '__main__':
 	main()
