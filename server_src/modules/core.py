6d344267a6b053e1f316050661a8c33908f6572d
Jason Chuang <jcchuang@sc.stanford.edu>
2014-02-24 22:30:21 -0700

    Updated allowed-origin

--- server_src/modules/core.py
+++ server_src/modules/core.py
@@ -147,8 +147,9 @@ class TermiteCore:
 		dataStr = json.dumps( data, encoding = 'utf-8', indent = 2, sort_keys = True )
 		
 		# Workaround while we build up the server-client architecture
-		acceptedIPs = [ 'http://' + self.request.env['REMOTE_ADDR'] + ':8080', 'http://127.0.0.1:8000' ]
-		self.response.headers['Access-Control-Allow-Origin'] = ', '.join(acceptedIPs)
+#		acceptedIPs = [ 'http://' + self.request.env['REMOTE_ADDR'] + ':8080', 'http://127.0.0.1:8000' ]
+#		self.response.headers['Access-Control-Allow-Origin'] = ', '.join(acceptedIPs)
+		self.response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:8000'
 		if self.IsJsonFormat():
 			return dataStr
 		else:
