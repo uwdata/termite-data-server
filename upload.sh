4082a4eed69c5d4c076b2721b6b19d8e08c868c5
Jason Chuang <jcchuang@cs.stanford.edu>
2014-03-03 11:01:14 -0800

    Updated path in upload.sh.

--- upload.sh
+++ upload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "rsync -ravz apps chuangca@jcchuang.org:workspace/TermiteTreeTM_GitHub/"
-rsync -ravz apps chuangca@jcchuang.org:workspace/TermiteTreeTM_GitHub/
+echo "rsync -ravz apps chuangca@jcchuang.org:workspace/TermiteSTM_GitHub/"
+rsync -ravz apps chuangca@jcchuang.org:workspace/TermiteSTM_GitHub/
 
-echo "rsync -ravz data chuangca@jcchuang.org:workspace/TermiteTreeTM_GitHub/"
-rsync -ravz data chuangca@jcchuang.org:workspace/TermiteTreeTM_GitHub/
+echo "rsync -ravz data chuangca@jcchuang.org:workspace/TermiteSTM_GitHub/"
+rsync -ravz data chuangca@jcchuang.org:workspace/TermiteSTM_GitHub/
 
-echo "rsync -ravz web2py/applications chuangca@jcchuang.org:workspace/TermiteTreeTM_GitHub/web2py/"
-rsync -ravz web2py/applications chuangca@jcchuang.org:workspace/TermiteTreeTM_GitHub/web2py/
+echo "rsync -ravz web2py/applications chuangca@jcchuang.org:workspace/TermiteSTM_GitHub/web2py/"
+rsync -ravz web2py/applications chuangca@jcchuang.org:workspace/TermiteSTM_GitHub/web2py/
