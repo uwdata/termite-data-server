53fbf50db54e870af4f162c9ab821709e55bfb06
Jason Chuang <jcchuang@cs.stanford.edu>
2014-03-17 18:31:21 -0700

    Poliblogs working with MALLET and Gensim.

--- bin/derive_poliblogs.py
+++ bin/derive_poliblogs.py
@@ -12,7 +12,7 @@ def ReadCSV( filename ):
 		documents = {}
 		reader = csv.reader( f, delimiter = ',', quotechar = '"' )
 		for index, values in enumerate( reader ):
-			values = [ d.decode( 'utf-8', 'replace' ) for d in values ]
+			values = [ d.decode( 'ascii', 'ignore' ) for d in values ]
 			if header is None:
 				header = values
 			else:
@@ -32,7 +32,7 @@ def ReadCSV( filename ):
 
 def Sanitize( header, documents ):
 	ALPHANUMERIC = re.compile( r'[\W]+' );
-	SINGLE_LINE = re.compile( r'[\n\r\f]+' );
+	SINGLE_LINE = re.compile( r'[\n\r\f\t]+' );
 	for i, value in enumerate( header ):
 		header[ i ] = ALPHANUMERIC.sub( '', value )
 	for document in documents.itervalues():
@@ -41,8 +41,14 @@ def Sanitize( header, documents ):
 	
 def WriteTSV( filename, header, keys, documents ):
 	with open( filename, 'w' ) as f:
+		for key in keys:
+			record = documents[ key ]
+			f.write( u'{}\t{}\n'.format( record['DocID'], record['DocContent'] ).encode( 'utf-8' ) )
+
+def WriteMeta( filename, header, keys, documents ):
+	with open( filename, 'w' ) as f:
 		f.write( u'{}\n'.format( u'\t'.join( header ) ).encode( 'utf-8' ) )
-		
+
 		for key in keys:
 			record = documents[ key ]
 			values = [ record[i] for i in header ]
@@ -52,11 +58,13 @@ def main():
 	parser = argparse.ArgumentParser( description = 'Convert a comma-separated file to tab-delimited file.' )
 	parser.add_argument( 'input' , type = str, nargs = '?', default = 'data/demo/poliblogs/corpus/poliblogs2008.csv', help = 'Input filename'  )
 	parser.add_argument( 'output', type = str, nargs = '?', default = 'data/demo/poliblogs/corpus/poliblogs2008.txt', help = 'Output filename' )
+	parser.add_argument( 'meta'  , type = str, nargs = '?', default = 'data/demo/poliblogs/corpus/poliblogs2008-meta.txt', help = 'Output filename' )
 	args = parser.parse_args()
 	
 	header, keys, documents = ReadCSV( args.input )
 	header, documents = Sanitize( header, documents )
 	WriteTSV( args.output, header, keys, documents )
+	WriteMeta( args.meta, header, keys, documents )
 
 if __name__ == '__main__':
 	main()
