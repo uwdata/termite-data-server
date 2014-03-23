7f9f70430d970ce76d2f24ed9c53fc5c0b3b028f
Jason Chuang <jcchuang@cs.stanford.edu>
2014-03-23 15:22:30 -0700

    Updated set2py setup script to reflect changes in applications folder structure.

--- bin/setup_web2py.sh
+++ bin/setup_web2py.sh
@@ -13,15 +13,6 @@ then
 	exit -1
 fi
 
-function __create_folder__ {
-	FOLDER=$1
-	if [ ! -d $FOLDER ]
-	then
-		echo "    Creating folder: $FOLDER"
-		mkdir $FOLDER
-	fi
-}
-
 function __setup_web2py__ {
 	EXTERNALS_SUBPATH=$EXTERNALS_PATH/web2py
 
@@ -31,8 +22,8 @@ function __setup_web2py__ {
 
 		if [ ! -d "$EXTERNALS_SUBPATH" ]
 		then
-			__create_folder__ $EXTERNALS_PATH
-			__create_folder__ $EXTERNALS_SUBPATH
+			mkdir -p $EXTERNALS_PATH
+			mkdir -p $EXTERNALS_SUBPATH
 			echo "    Downloading..."
 			curl --insecure --location http://www.web2py.com/examples/static/web2py_src.zip > $EXTERNALS_SUBPATH/web2py_src.zip
 			echo "    Extracting license..."
@@ -43,8 +34,7 @@ function __setup_web2py__ {
 			echo "    Already downloaded: $EXTERNALS_SUBPATH/web2py_src.zip"
 		fi
 
-		__create_folder__ $APPS_PATH
-		__create_folder__ $WEB2PY_PATH
+		mkdir -p $WEB2PY_PATH
 		echo "    Uncompressing..."
 		unzip -q $EXTERNALS_SUBPATH/web2py_src.zip web2py/* -d $WEB2PY_PATH &&\
 			mv $WEB2PY_PATH/web2py/* $WEB2PY_PATH/ &&\
@@ -52,25 +42,42 @@ function __setup_web2py__ {
 		
 		echo "    Removing 'no password, no web admin interface' dialogue box..."
 		sed -i bkp "s/self.error('no password, no web admin interface')/pass #self.error('no password, no web admin interface')/g" $WEB2PY_PATH/gluon/widget.py
-		
-		echo "    Removing unused apps and example files..."
+
+		echo "    Removing unused apps..."
 		rm -rf $WEB2PY_PATH/applications/welcome
 		rm -rf $WEB2PY_PATH/applications/examples
 		rm -rf $WEB2PY_PATH/examples
 		rm -rf $WEB2PY_PATH/handlers
 		
-		__create_folder__ $WEB2PY_PATH/applications/init
-		echo "    Creating the default app..."
-		touch $WEB2PY_PATH/applications/init/__init__.py
-		ln -s ../../../landing_src/views $WEB2PY_PATH/applications/init/views
-		ln -s ../../../landing_src/models $WEB2PY_PATH/applications/init/models
-		ln -s ../../../landing_src/controllers $WEB2PY_PATH/applications/init/controllers
-		ln -s ../../../landing_src/modules $WEB2PY_PATH/applications/init/modules
-		ln -s ../../../landing_src/static $WEB2PY_PATH/applications/init/static
-		
 		echo "    Copying FastCGI setup..."
 		cp web2py_src/* $WEB2PY_PATH/
 		cp web2py_src/.htaccess $WEB2PY_PATH/
+		
+		if [ ! -d $APPS_PATH ]
+		then
+			echo "    Creating $APPS_PATH folder..."
+			mkdir $APPS_PATH
+		fi
+
+		if [ ! -d $APPS_PATH/init ]
+		then
+			echo "    Creating the default app..."
+			mkdir $APPS_PATH/init
+			touch $APPS_PATH/init/__init__.py
+			ln -s ../../landing_src/views $APPS_PATH/init/views
+			ln -s ../../landing_src/models $APPS_PATH/init/models
+			ln -s ../../landing_src/controllers $APPS_PATH/init/controllers
+			ln -s ../../landing_src/modules $APPS_PATH/init/modules
+			ln -s ../../landing_src/static $APPS_PATH/init/static
+		fi
+		
+		if [ ! -d $WEB2PY_PATH/applications-original ]
+		then
+			echo "    Linking to $APPS_PATH folder..."
+			mv $WEB2PY_PATH/applications $WEB2PY_PATH/applications-original
+			ln -s ../$APPS_PATH/ $WEB2PY_PATH/applications
+			ln -s ../$WEB2PY_PATH/applications-original/admin/ $APPS_PATH/admin
+		fi
 	else
 		echo "    Already available: $WEB2PY_PATH"
 	fi
