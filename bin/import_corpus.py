8718706ff382c498939fa07b70d67f7fd7eee716
Jason Chuang <jcchuang@cs.stanford.edu>
2014-03-25 23:10:48 -0700

    Added g2 statistics.

--- bin/import_corpus.py
+++ bin/import_corpus.py
@@ -4,6 +4,7 @@
 import math
 import json
 import sqlite3
+from collections import Counter
 from import_abstr import ImportAbstraction
 from corpus.CorpusReader import CorpusReader
 from corpus.MetaReader import MetaReader
@@ -75,8 +76,8 @@ class ImportCorpus( ImportAbstraction ):
 		print '    Reading text corpus: {}'.format( filename )
 		reader = CorpusReader( filename, stopwords = self.stopwords )
 		corpus = { docID : docTokens for docID, docContent, docTokens in reader.Load() }
-		termStats, termFreqs, termProbs, termDocFreqs = self.ComputeTermFreqs( corpus )
-		termCoStats = self.ComputeTermCoFreqs( corpus, termFreqs, termProbs, termDocFreqs, minFreq, minDocFreq, maxCount )
+		termStats = self.ComputeTermFreqs( corpus )
+		termCoStats = self.ComputeTermCoFreqs( corpus, termStats, minFreq, minDocFreq, maxCount )
 		termFreqSortedList, termTfIdfSortedList = self.CompileTermKeys( termStats )
 		self.SaveCorpusStatsToDisk( termStats, termCoStats, termFreqSortedList, termTfIdfSortedList )
 	
@@ -85,98 +86,178 @@ class ImportCorpus( ImportAbstraction ):
 		print '    Reading text corpus: {}'.format( filename )
 		reader = SentenceReader( filename, stopwords = self.stopwords )
 		corpus = { docID+":"+docSubID : docTokens for docID, docSubID, docSentence, docTokens in reader.Load() }
-		termStats, termFreqs, termProbs, termDocFreqs = self.ComputeTermFreqs( corpus )
-		termCoStats = self.ComputeTermCoFreqs( corpus, termFreqs, termProbs, termDocFreqs, minFreq, minDocFreq, maxCount )
+		termStats = self.ComputeTermFreqs( corpus )
+		termCoStats = self.ComputeTermCoFreqs( corpus, termStats, minFreq, minDocFreq, maxCount )
 		termFreqSortedList, termTfIdfSortedList = self.CompileTermKeys( termStats )
 		self.SaveSentenceStatsToDisk( termStats, termCoStats, termFreqSortedList, termTfIdfSortedList )
 	
 	def ComputeTermFreqs( self, corpus ):
+		def ComputeFreqs( corpus ):
+			freqs = Counter()
+			for docID, docTokens in corpus.iteritems():
+				freqs.update( docTokens )
+			return freqs
+		
+		def ComputeDocFreqs( corpus ):
+			docFreqs = Counter()
+			for docID, docTokens in corpus.iteritems():
+				uniqueTokens = frozenset( docTokens )
+				docFreqs.update( uniqueTokens )
+			return docFreqs
+		
+		def NormalizeFreqs( freqs ):
+			normalization = sum( freqs.itervalues() )
+			normalization = 1.0 / normalization if normalization > 1.0 else 1.0
+			probs = { term : freq * normalization for term, freq in freqs.iteritems() }
+			return probs
+		
+		def ComputeIdfs( corpus, docFreqs ):
+			N = len( corpus )
+			idfs = { term : math.log( 1.0 * N / docFreq ) for term, docFreq in docFreqs.iteritems() }
+			return idfs
+		
+		def ComputeTfIdfs( freqs, idfs ):
+			tfIdfs = { term : freq * idfs[term] for term, freq in freqs.iteritems() }
+			return tfIdfs
+			
 		print '    Computing term freqs ({})...'.format( len(corpus) )
-		termFreqs = {}
-		termDocFreqs = {}
-		for docID, docTokens in corpus.iteritems():
-			for token in docTokens:
-				if token not in termFreqs:
-					termFreqs[ token ] = 1
-				else:
-					termFreqs[ token ] += 1
-			uniqueTokens = frozenset( docTokens )
-			for token in uniqueTokens:
-				if token not in termDocFreqs:
-					termDocFreqs[ token ] = 1
-				else:
-					termDocFreqs[ token ] += 1
-
-		# Normalize to create a probability distribution
-		normalization = sum( termFreqs.itervalues() )
-		normalization = 1.0 / normalization if normalization > 1.0 else 1.0
-		termProbs = { term : termFreqs[term] * normalization for term in termFreqs.iterkeys() }
-
-		N = len(corpus)
-		termTfIdfs = { term : termFreqs[term] * math.log(1.0*N/termDocFreqs[term]) for term in termFreqs.iterkeys() }
+		termFreqs = ComputeFreqs( corpus )
+		termDocFreqs = ComputeDocFreqs( corpus )
+		termProbs = NormalizeFreqs( termFreqs )
+		termIdfs = ComputeIdfs( corpus, termDocFreqs )
+		termTfIdfs = ComputeTfIdfs( termFreqs, termIdfs )
 		
 		termStats = {
 			'freqs' : termFreqs,
 			'probs' : termProbs,
 			'docFreqs' : termDocFreqs,
+			'idfs' : termIdfs,
 			'tfIdfs' : termTfIdfs
 		}
-		return termStats, termFreqs, termProbs, termDocFreqs
+		return termStats
 
-	def ComputeTermCoFreqs( self, corpus, termFreqs, termProbs, termDocFreqs, minFreq, minDocFreq, maxCount ):
-		def getTokenPairs( firstToken, secondToken ):
+	def ComputeTermCoFreqs( self, corpus, termStats, minFreq, minDocFreq, maxCount ):
+		termFreqs = termStats['freqs']
+		termProbs = termStats['probs']
+		termDocFreqs = termStats['docFreqs']
+		
+		def GetVocab( termFreqs, termDocFreqs, minFreq, minDocFreq, maxCount ):
+			keys = [ term for term in termFreqs if termFreqs[term] >= minFreq and termDocFreqs[term] >= minDocFreq ]
+			keys = sorted( keys, key = lambda x : -termFreqs[x] )
+			keys = keys[:maxCount]
+			return frozenset(keys)
+			
+		def GetTokenPairs( firstToken, secondToken ):
 			if firstToken < secondToken:
 				return firstToken, secondToken
 			else:
 				return secondToken, firstToken
-				
-		print '    Computing term co-occurrences ({})...'.format( len(corpus) )
-		keys = set()
-		for term in termFreqs:
-			if termFreqs[term] >= minFreq:
-				if termDocFreqs[term] >= minDocFreq:
-					keys.add(term)
-		keys = sorted( keys, key = lambda x : -termFreqs[x] )
-		keys = keys[:maxCount]
-		keySet = frozenset(keys)
-		termCoFreqs = {}
-		for docID, docTokens in corpus.iteritems():
-			n = len(docTokens)
-			for i in range(n):
-				firstToken = docTokens[i]
-				if firstToken in keySet:
-					for j in range(i+1,n):
-						secondToken = docTokens[j]
-						if secondToken in keySet:
-							a, b = getTokenPairs( firstToken, secondToken )
-							if a not in termCoFreqs:
-								termCoFreqs[a] = { b : 1 }
-							elif b not in termCoFreqs[a]:
-								termCoFreqs[a][b] = 1
-							else:
-								termCoFreqs[a][b] += 1
 
-		# Normalize to create joint probability distribution
-		normalization = sum( sum( d.itervalues() ) for d in termCoFreqs.itervalues() )
-		normalization = 1.0 / normalization if normalization > 1.0 else 1.0
-		termCoProbs = { term : { t : f * normalization for t, f in termFreqs.iteritems() } for term, termFreqs in termCoFreqs.iteritems() }
+		def ComputeJointFreqs( corpus, vocab ):
+			jointFreqs = {}
+			allFreq = 0
+			allCoFreq = 0
+			for docID, docTokens in corpus.iteritems():
+				freqs = Counter( docTokens )
+				allFreq += sum( freqs.itervalues() )
+				for firstToken, firstFreq in freqs.iteritems():
+					for secondToken, secondFreq in freqs.iteritems():
+						coFreq = firstFreq * secondFreq
+						allCoFreq += coFreq
+						if firstToken in vocab and secondToken in vocab and firstToken <= secondToken:
+							if firstToken not in jointFreqs:
+								jointFreqs[firstToken] = { secondToken : coFreq }
+							elif secondToken not in jointFreqs[firstToken]:
+								jointFreqs[firstToken][secondToken] = coFreq
+							else:
+								jointFreqs[firstToken][secondToken] += coFreq
+			return jointFreqs, allFreq, allCoFreq
 		
-		termPMI = {}
-		for x, probs in termCoProbs.iteritems():
-			termPMI[x] = {}
-			for y, prob in probs.iteritems():
-				if x in termProbs and y in termProbs:
-					termPMI[x][y] = prob / termProbs[x] / termProbs[y]
-				else:
-					termPMI[x][y] = 0.0
+		def NormalizeJointFreqs( jointFreqs, normalization = None ):
+			if normalization is None:
+				normalization = sum( sum( d.itervalues() ) for d in jointFreqs.itervalues() )
+				normalization = 1.0 / normalization if normalization > 1.0 else 1.0
+			jointProbs = { term : { t : f * normalization for t, f in freqs.iteritems() } for term, freqs in jointFreqs.iteritems() }
+			return jointProbs
+
+		def ComputePMI( marginalProbs, jointProbs ):
+			pmi = {}
+			for x, probs in jointProbs.iteritems():
+				pmi[x] = {}
+				for y, prob in probs.iteritems():
+					if x in marginalProbs and y in marginalProbs:
+						pmi[x][y] = prob / marginalProbs[x] / marginalProbs[y]
+					else:
+						pmi[x][y] = 0.0
+			return pmi
+		
+		def GetBinomial( B_given_A, any_given_A, B_given_notA, any_given_notA ):
+			assert B_given_A >= 0
+			assert B_given_notA >= 0
+			assert any_given_A >= B_given_A
+			assert any_given_notA >= B_given_notA
+
+			a = float( B_given_A )
+			b = float( B_given_notA )
+			c = float( any_given_A )
+			d = float( any_given_notA )
+			E1 = c * ( a + b ) / ( c + d )
+			E2 = d * ( a + b ) / ( c + d )
+			g2a = a * math.log( a / E1 ) if a > 0 else 0
+			g2b = b * math.log( b / E2 ) if b > 0 else 0
+			return 2 * ( g2a + g2b )
+
+		def GetG2Stats( freq_all, freq_ab, freq_a, freq_b ):
+			assert freq_all >= freq_a
+			assert freq_all >= freq_b
+			assert freq_a >= freq_ab
+			assert freq_b >= freq_ab
+			assert freq_all >= 0
+			assert freq_ab >= 0
+			assert freq_a >= 0
+			assert freq_b >= 0
+
+			B_given_A = freq_ab
+			B_given_notA = freq_b - freq_ab
+			any_given_A = freq_a
+			any_given_notA = freq_all - freq_a
+			return GetBinomial( B_given_A, any_given_A, B_given_notA, any_given_notA )
+
+		def ComputeG2Stats( allFreq, marginalProbs, jointProbs ):
+			g2_stats = {}
+			freq_all = allFreq
+			for firstToken, d in jointProbs.iteritems():
+				g2_stats[ firstToken ] = {}
+				for secondToken, jointProb in d.iteritems():
+					freq_a = allFreq * marginalProbs[ firstToken ]
+					freq_b = allFreq * marginalProbs[ secondToken ]
+					freq_ab = allFreq * jointProb
+					g2_stats[ firstToken ][ secondToken ] = GetG2Stats( freq_all, freq_ab, freq_a, freq_b )
+			return g2_stats
+		
+		print '    Computing term co-occurrences ({})...'.format( len(corpus) )
+		vocab = GetVocab( termFreqs, termDocFreqs, minFreq, minDocFreq, maxCount )
+
+		# Count co-occurrences
+		termCoFreqs, allFreq, allCoFreq = ComputeJointFreqs( corpus, vocab )
+		
+		# Normalize to create joint probability distribution
+		termCoProbs = NormalizeJointFreqs( termCoFreqs, 1.0 / allCoFreq if allCoFreq > 1.0 else 1.0 )
+		
+		# Compute pointwise mutual information from marginal/joint probability distributions
+		termPMI = ComputePMI( termProbs, termCoProbs )
+		
+		# Compute G2 statistics
+		termG2 = ComputeG2Stats( allFreq, termProbs, termCoProbs )
 		
 		termCoStats = {
 			'coFreqs' : termCoFreqs,
 			'coProbs' : termCoProbs,
-			'pmi' : termPMI
+			'pmi' : termPMI,
+			'g2' : termG2
 		}
 		return termCoStats
-	
+			
 	def CompileTermKeys( self, termStats ):
 		termFreqSortedList = sorted( termStats['freqs'].iterkeys(), key = lambda x : -termStats['freqs'][x] )
 		termTfIdfSortedList = sorted( termStats['tfIdfs'].iterkeys(), key = lambda x : -termStats['tfIdfs'][x] )
