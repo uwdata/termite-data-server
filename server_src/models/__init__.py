ab7e16daa27a5bf29fd4d22c77330541b2bd5bad
Jason Chuang <jcchuang@cs.stanford.edu>
2014-04-25 00:19:41 -0700

    Fixed path issues for command line and web2py apps.

--- server_src/models/__init__.py
+++ server_src/models/__init__.py
@@ -2,8 +2,11 @@
 
 __all__ = [
 	'001_unicode',
-	'Corpus_DB',
-	'CorpusStats_DB',
-	'LDA_DB',
-	'LDAStats_DB'
+	'Corpus_DB', 'CorpusStats_DB',
+	'LDA_DB', 'LDAStats_DB'
 ]
+
+from Corpus_DB import Corpus_DB
+from CorpusStats_DB import CorpusStats_DB
+from LDA_DB import LDA_DB
+from LDAStats_DB import LDAStats_DB
