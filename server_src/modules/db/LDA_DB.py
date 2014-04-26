8257de7566f116798083cd96eeed34a4e99f8179
Jason Chuang <jcchuang@cs.stanford.edu>
2014-04-26 01:26:05 -0700

    Updated LDA_DB

--- server_src/modules/db/LDA_DB.py
+++ server_src/modules/db/LDA_DB.py
@@ -9,37 +9,59 @@ class LDA_DB():
 	
 	def __init__(self, path = None, isInit = False):
 		self.isInit = isInit
-		self.isLazy = not isInit
 		if path is not None:
-			self.db = DAL( LDA_DB.CONNECTION, lazy_tables = self.isLazy, folder = path )
+			self.db = DAL( LDA_DB.CONNECTION, lazy_tables = not self.isInit, migrate = self.isInit, folder = path )
 		else:
-			self.db = DAL( LDA_DB.CONNECTION, lazy_tables = self.isLazy )
+			self.db = DAL( LDA_DB.CONNECTION, lazy_tables = not self.isInit, migrate = self.isInit )
+
+	def __enter__(self):
+		self.DefineDimensionTables()
+		self.DefineMatrixTables()
+		return self
 	
-	def DefineTables(self):
+	def __exit__(self, type, value, traceback):
+		self.db.commit()
+	
+	def DefineDimensionTables(self):
 		self.db.define_table( 'terms',
 			Field( 'term_index', 'integer', required = True, unique = True, default = -1 ),
 			Field( 'term_text' , 'string' , required = True, unique = True ),
-			redefine = False
+			Field( 'term_freq' , 'double' , required = True ),
+			Field( 'rank'      , 'integer', required = True )
 		)
+		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS terms_index ON terms (term_index);' )
+		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS terms_text  ON terms (term_text);' )
+		self.db.executesql( 'CREATE        INDEX IF NOT EXISTS terms_freq  ON terms (term_freq);' )
+		self.db.executesql( 'CREATE        INDEX IF NOT EXISTS terms_rank  ON terms (rank);' )
 		self.db.define_table( 'docs',
 			Field( 'doc_index', 'integer', required = True, unique = True, default = -1 ),
-			redefine = False
+			Field( 'doc_id'   , 'string' , required = True, unique = True ),
+			Field( 'doc_freq' , 'double' , required = True ),
+			Field( 'rank'     , 'integer', required = True )
 		)
+		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS docs_index ON docs (doc_index);' )
+		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS docs_id    ON docs (doc_id);' )
+		self.db.executesql( 'CREATE        INDEX IF NOT EXISTS docs_freq  ON docs (doc_freq);' )
+		self.db.executesql( 'CREATE        INDEX IF NOT EXISTS docs_rank  ON docs (rank);' )
 		self.db.define_table( 'topics',
-			Field( 'topic_index', 'integer', required = True, unique = True, default = -1 ),
-			Field( 'topic_freq' , 'double' , required = True ),
-			Field( 'topic_desc' , 'string' , required = True ),
-			Field( 'topic_top_terms', 'list:string', required = True ),
-			redefine = False
+			Field( 'topic_index', 'integer'     , required = True, unique = True, default = -1 ),
+			Field( 'topic_freq' , 'double'      , required = True ),
+			Field( 'topic_label', 'string'      , required = True ),
+			Field( 'topic_desc' , 'string'      , required = True ),
+			Field( 'top_terms'  , 'list:integer', required = True ),
+			Field( 'top_docs'   , 'list:integer', required = True ),
+			Field( 'rank'       , 'integer'     , required = True )
 		)
-		self.db.executesql( 'CREATE INDEX IF NOT EXISTS topics_freq ON topics (topic_freq);' )
-		
+		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS topics_index ON topics (topic_index);' )
+		self.db.executesql( 'CREATE        INDEX IF NOT EXISTS topics_freq  ON topics (topic_freq);' )
+		self.db.executesql( 'CREATE        INDEX IF NOT EXISTS topics_rank  ON topics (rank);' )
+
+	def DefineMatrixTables(self):
 		self.db.define_table( 'term_topic_matrix',
 			Field( 'term_index' , 'integer', required = True, default = -1 ),
 			Field( 'topic_index', 'integer', required = True, default = -1 ),
 			Field( 'value', 'double' , required = True ),
-			Field( 'rank' , 'integer', required = True ),
-			redefine = False
+			Field( 'rank' , 'integer', required = True )
 		)
 		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS term_topic_indexes ON term_topic_matrix (term_index, topic_index);' )
 		self.db.executesql( 'CREATE INDEX IF NOT EXISTS term_topic_value      ON term_topic_matrix (value);' )
@@ -51,18 +73,10 @@ class LDA_DB():
 			Field( 'doc_index'  , 'integer', required = True, default = -1 ),
 			Field( 'topic_index', 'integer', required = True, default = -1 ),
 			Field( 'value', 'double' , required = True ),
-			Field( 'rank' , 'integer', required = True ),
-			redefine = False
+			Field( 'rank' , 'integer', required = True )
 		)
 		self.db.executesql( 'CREATE UNIQUE INDEX IF NOT EXISTS doc_topic_indexes ON doc_topic_matrix (doc_index, topic_index);' )
 		self.db.executesql( 'CREATE INDEX IF NOT EXISTS doc_topic_value      ON doc_topic_matrix (value);' )
 		self.db.executesql( 'CREATE INDEX IF NOT EXISTS doc_topic_rank       ON doc_topic_matrix (rank);' )
 		self.db.executesql( 'CREATE INDEX IF NOT EXISTS doc_topic_docindex   ON doc_topic_matrix (doc_index);' )
 		self.db.executesql( 'CREATE INDEX IF NOT EXISTS doc_topic_topicindex ON doc_topic_matrix (topic_index);' )
-	
-	def __enter__(self):
-		self.DefineTables()
-		return self
-	
-	def __exit__(self, type, value, traceback):
-		self.db.commit()
