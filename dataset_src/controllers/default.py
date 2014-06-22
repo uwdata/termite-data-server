3f461e90c6b2bbcec94d7656ddd4562e9b720768
Peter Enns <peter@mojo.local>
2014-06-22 12:02:12 -0700

    Corpus uploading

--- dataset_src/controllers/default.py
+++ dataset_src/controllers/default.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import os
+import utils.uploads as uploads
 
 def index():
-	return {}
+    corpora = [fname[:-len(".csv")] for fname in os.listdir(uploads.spreadsheet_dir(request))]
+    return {"corpora": corpora}
--- dataset_src/controllers/download.py
+++ dataset_src/controllers/download.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+import os
+import utils.paths as paths
 
 def index():
-	return {}
+    corpus = request.vars['corpus']
+    return {"csv": "/dataset/download/csv?corpus=" + corpus + ".csv",
+            "ptext": "/dataset/download/plaintext?corpus=" +  corpus + ".txt"}
+
+def csv():
+    corpus = request.vars['corpus']
+    with open(os.path.join(paths.spreadsheet(request), corpus)) as f:
+        return f.read()
+
+def plaintext():
+    corpus = request.vars['corpus']
+    with open(os.path.join(paths.plaintext(request), corpus)) as f:
+        return f.read()
