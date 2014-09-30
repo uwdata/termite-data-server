72c6f555b86a001ee22c9c9001d7f54c6249f279
Jason Chuang <jcchuang@cs.stanford.edu>
2014-09-30 12:13:27 -0400

    Added an extra flag to GensimReader to skip document index.

--- server_src/modules/readers/GensimReader.py
+++ server_src/modules/readers/GensimReader.py
@@ -13,14 +13,16 @@ class GensimReader(LDAReader):
 	DICTIONARY_FILENAME = 'dictionary.gensim'
 	CORPUS_FILENAME = 'corpus.gensim'
 	MODEL_FILENAME = 'lda.gensim'
+	STATE_FILENAME = 'state.gensim'
 
-	def __init__( self, lda_db, modelPath, corpus_db ):
+	def __init__( self, lda_db, modelPath, corpus_db, extraStateFile = False ):
 		super(GensimReader, self).__init__(lda_db)
 		self.modelPath = modelPath
 		self.dictionaryInGensim = '{}/{}'.format( self.modelPath, GensimReader.DICTIONARY_FILENAME )
-		self.corpusInGensim = '{}/{}'.format( self.modelPath, GensimReader.CORPUS_FILENAME )
+		self.corpusInGensim = '{}/{}'.format( self.modelPath, GensimReader.CORPUS_FILENAME ) if corpus_db is not None else None 
 		self.modelInGensim = '{}/{}'.format( self.modelPath, GensimReader.MODEL_FILENAME )
-		self.corpus = corpus_db.db
+		self.stateInGensim = '{}/{}'.format( self.modelPath, GensimReader.STATE_FILENAME ) if extraStateFile else None
+		self.corpus = corpus_db.db if corpus_db is not None else None
 		
 	def Execute( self ):
 		self.logger.info( 'Reading gensim LDA output...' )
@@ -32,12 +34,14 @@ class GensimReader(LDAReader):
 		self.logger.debug( '    Loading dictionary: %s', self.dictionaryInGensim )
 		self.dictionary = corpora.Dictionary.load( self.dictionaryInGensim )
 		self.logger.debug( '    Loading corpus: %s', self.corpusInGensim )
-		self.text_corpus = corpora.TextCorpus.load( self.corpusInGensim )
+		self.text_corpus = corpora.TextCorpus.load( self.corpusInGensim ) if self.corpus is not None else None 
 		self.logger.debug( '    Loading model: %s', self.modelInGensim )
 		self.model = models.LdaModel.load( self.modelInGensim )
+		if self.stateInGensim is not None:
+			self.model.state = models.ldamodel.LdaState.load( self.stateInGensim )
 		
 		self.termList = [ self.dictionary[index] for index in self.dictionary ]
-		self.docList = [ d.doc_id for d in self.corpus().select(self.corpus.corpus.doc_id, orderby=self.corpus.corpus.doc_index) ]
+		self.docList = [ d.doc_id for d in self.corpus().select(self.corpus.corpus.doc_id, orderby=self.corpus.corpus.doc_index) ] if self.corpus is not None else None
 
 		self.termTopicMatrix = []
 		topicsAndFreqsTerms = self.model.show_topics(topics=-1, topn=len(self.dictionary), formatted=False)
@@ -56,16 +60,17 @@ class GensimReader(LDAReader):
 			d['rank'] = index + 1
 		
 		self.docTopicMatrix = []
-		docsAndDocBOWs = self.text_corpus
-		for docIndex, docBOW in enumerate(docsAndDocBOWs):
-			topicsProbs = self.model[docBOW]
-			for topicIndex, value in topicsProbs:
-				self.docTopicMatrix.append({
-					'doc_index' : docIndex,
-					'topic_index' : topicIndex,
-					'value' : value,
-					'rank' : 0
-				})
+		if self.corpus is not None:
+			docsAndDocBOWs = self.text_corpus
+			for docIndex, docBOW in enumerate(docsAndDocBOWs):
+				topicsProbs = self.model[docBOW]
+				for topicIndex, value in topicsProbs:
+					self.docTopicMatrix.append({
+						'doc_index' : docIndex,
+						'topic_index' : topicIndex,
+						'value' : value,
+						'rank' : 0
+					})
 		self.docTopicMatrix.sort( key = lambda d : -d['value'] )
 		for index, d in enumerate(self.docTopicMatrix):
 			d['rank'] = index + 1
